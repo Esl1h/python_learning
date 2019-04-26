2bcd19bc118bfeef69921a711c05bda514acac12
Esli Silva <esli@esli-nux.com>
2019-04-26 00:08:00 -0300

    exercises 07 - function

---
+++ 07-function_compareNumber.py
@@ -0,0 +1,12 @@
+#!/bin/python3.7
+# -*- coding: utf-8 -*-
+def comp(x,y):
+	if x == y:
+		print ("equals")
+	else:
+		print ("different")
+
+num1 = input("num1:")
+num2 = input("num2:")
+
+comp(num1,num2)
