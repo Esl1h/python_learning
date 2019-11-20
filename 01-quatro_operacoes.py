6fd9612c795aea38cfb3e6f50881db30afe53220
Esli Silva <esli@esli-nux.com>
2019-11-19 23:44:59 -0300

    moving to python3 ;-)

--- 01-quatro_operacoes.py
+++ 01-quatro_operacoes.py
@@ -1,15 +1,16 @@
-#!/bin/python2
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
 Created on Sun May  8 22:22:31 2016
 
 @author: esl1h
-Python2
+Python3
 """
-print """ Olá, Eu farei operações matematicas, usando os 4 operadores comuns através dos dois numeros que você me fornecer"""
-var1 = input ("primeiro número: ")
-var2 = input ("segundo número: ")
-print "Adição:", var1 + var2
-print "Subtração:", var1-var2
-print "Divisão inteira:", var1 / var2
-print "Multiplicação ", var1 * var2
+#print """ Olá, Eu farei operações matematicas, usando os 4 operadores comuns através dos dois numeros que você me fornecer"""
+print( """ Olá, Eu farei operações matematicas, usando os 4 operadores comuns através dos dois numeros que você me fornecer""")
+var1 = input("primeiro número: ")
+var2 = input("segundo número: ")
+print( "Adição:", int(var1) + int(var2))
+print( "Subtração:", int(var1) - int(var2))
+print( "Divisão inteira:", int(var1) / int(var2))
+print( "Multiplicação ", int(var1) * int(var2))
