6e6d24ffbc7debb33f72cfbdab58877606cca84e
Esli <esli@esli-nux.com>
2020-03-14 12:38:31 -0300

    OO example window kivy

---
+++ kivy/source/oo-example/main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 13:46:53 2020
+
+@author: esli
+"""
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class Program(App):
+
+    def build(selfself):
+        return Label()
+
+Program().run()
+
