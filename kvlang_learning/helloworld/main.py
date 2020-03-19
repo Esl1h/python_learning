b0f72f7fe9e01930d6e135a748f8cdd1955aa907
Esli <esli@esli-nux.com>
2020-03-19 13:22:46 -0300

    starting learn kivy, kvlang. Examples.

---
+++ kvlang_learning/helloworld/main.py
@@ -0,0 +1,20 @@
+#coding: utf-8
+
+# Simple hello world in python:
+
+# from kivy.app import App
+# from kivy.uix.label import Label
+#
+# class HelloApp(App):
+#
+#     def build(selfself):
+#         return Label(text="Hello World")
+#
+# HelloApp().run()
+
+from kivy.app import App
+
+class hello(App):
+    pass
+
+hello().run()
